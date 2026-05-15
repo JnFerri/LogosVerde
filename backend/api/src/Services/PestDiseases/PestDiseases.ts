47715b43efcf79c03eab47e397fb96c1cb93fff6
JnFerri <jeanferrimh@gmail.com>
2026-05-15 14:32:09 -0300

    refactor: unnecessary validations

--- backend/api/src/Services/PestDiseases/PestDiseases.ts
+++ backend/api/src/Services/PestDiseases/PestDiseases.ts
@@ -2,7 +2,7 @@
 import { ApiError } from "../../Models/ApiResponse/ApiError";
 import { PestDiseasesSchema } from "../../Models/DTO/PestDisease";
 import PestDiseasesRepository from "../../Repository/PestDiseases/PestDiseases"
-import type { PestDiseases, PestDiseasesCreate, PestDiseasesUpdate } from "../../Types/PestDiseases";
+import type { PestDiseases, PestDiseasesCreate, PestDiseasesIdParam, PestDiseasesUpdate } from "../../Types/PestDiseases";
 
 export class PestDiseaseService{
     constructor(
@@ -16,10 +16,7 @@ export class PestDiseaseService{
             return PestDiseasesSchema.array().parse(response)
     }
 
-    async findById(id: number){
-            if(!id){
-                throw ApiError.BadRequest("Id is required")
-            }
+    async findById(id: PestDiseasesIdParam){
             const response = await this.repository.getById(id)
             if(!response){
                 throw ApiError.NotFound("Pest disease not found")
@@ -32,23 +29,12 @@ export class PestDiseaseService{
             return PestDiseasesSchema.parse(response)
     }
 
-    async update(id: number, data: PestDiseasesUpdate){
-            if(!id){
-                throw ApiError.BadRequest("Id is required")
-            }
-            const exists = await this.repository.getById(id)
-            if (!exists) {
-                throw ApiError.NotFound("Pest disease not found")
-            }
+    async update(id: PestDiseasesIdParam, data: PestDiseasesUpdate){
             const response =  await this.repository.update(id, data)
             return PestDiseasesSchema.parse(response)
     }
 
-    async delete(id: number){
-            const exists = await this.repository.getById( id )
-            if (!exists) {
-                throw ApiError.NotFound("Pest disease not found")
-            }
+    async delete(id: PestDiseasesIdParam){
             await this.repository.delete(id)
     }
 }
