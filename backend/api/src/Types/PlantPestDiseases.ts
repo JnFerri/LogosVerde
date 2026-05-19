af57cad54d57a541477d3928112c39b3632fc518
JnFerri <jeanferrimh@gmail.com>
2026-05-19 14:56:12 -0300

    create: repository plantPestDiseases

--- backend/api/src/Types/PlantPestDiseases.ts
+++ backend/api/src/Types/PlantPestDiseases.ts
@@ -1,10 +1,10 @@
 import type z from "zod";
 import type { PlantPestDiseaseCreateSchema, PlantPestDiseaseParamIdPestDiseaseSchema, PlantPestDiseaseParamIdPlantSchema, PlantPestDiseaseSchema } from "../Models/DTO/PlantPestDiseases";
 
-export type PlantPestDisease = z.infer<typeof PlantPestDiseaseSchema>;
+export type PlantPestDiseases = z.infer<typeof PlantPestDiseaseSchema>;
 
-export type PlantPestDiseaseCreate = z.infer<typeof PlantPestDiseaseCreateSchema>;
+export type PlantPestDiseasesCreate = z.infer<typeof PlantPestDiseaseCreateSchema>;
 
-export type PlantPestDiseaseIdPlantParam = z.infer<typeof PlantPestDiseaseParamIdPlantSchema>;
+export type PlantPestDiseasesIdPlantParam = z.infer<typeof PlantPestDiseaseParamIdPlantSchema>;
 
-export type PlantPestDiseaseIdPestDiseaseParam = z.infer<typeof PlantPestDiseaseParamIdPestDiseaseSchema>;
+export type PlantPestDiseasesIdPestDiseaseParam = z.infer<typeof PlantPestDiseaseParamIdPestDiseaseSchema>;
