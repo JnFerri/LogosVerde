9d7f25451c18f316ee57eb389c2e9ad332211d14
JnFerri <jeanferrimh@gmail.com>
2026-05-28 16:35:00 -0300

    refactor: change entity for plantingAreas

--- frontend/src/features/PlantingAreas/Entities/PlantingArea.ts
+++ frontend/src/features/PlantingAreas/Entities/PlantingArea.ts
@@ -1,9 +1,21 @@
-export default class PlantingArea {  
+export default class PlantingArea {
+  public readonly id: number;
+  public name: string;
+  public sunshineHours: number | null;
+  public projectId: number;
+  public createdAt: Date;
+
   constructor(
-    public readonly id: number,
-    public name: string,
-    public sunshineHours : number | null,
-    public projectId : number,
-    public createdAt: Date
-  ){}
+    id: number,
+    name: string,
+    sunshineHours: number | null,
+    projectId: number,
+    createdAt: Date
+  ) {
+    this.id = id;
+    this.name = name;
+    this.sunshineHours = sunshineHours;
+    this.projectId = projectId;
+    this.createdAt = createdAt;
+  }
 }
--- frontend/src/features/PlantingAreas/Types/PlantingAreas.ts
+++ frontend/src/features/PlantingAreas/Types/PlantingAreas.ts
@@ -1,5 +1,5 @@
 import type z from "zod";
-import type { PlantingAreaCreateSchema, PlantingAreaIdParamSchema, PlantingAreaUpdateSchema } from "../../../Schemas/PlantingAreas";
+import type { PlantingAreaCreateSchema, PlantingAreaIdParamSchema, PlantingAreaUpdateSchema } from "../Schemas/PlantingAreas";
 import type PlantingAreaPlant from "../../../Entities/PlantingAreaPlant/PlantingAreaPlant.entity";
 import type PlantingArea from "../Entities/PlantingArea";
 import type PlantingAreaFertilizing from "../../../Entities/PlantingAreaFertilizing/PlantingAreaFertilizing.entity";
