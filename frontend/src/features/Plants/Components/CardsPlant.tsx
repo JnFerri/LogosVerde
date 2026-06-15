b07e630f3f82a407c40f3e0f96a375cd975450d3
JnFerri <jeanferrimh@gmail.com>
2026-06-15 07:57:43 -0300

    fix: remove console

--- frontend/src/features/Plants/Components/CardsPlant.tsx
+++ frontend/src/features/Plants/Components/CardsPlant.tsx
@@ -31,44 +31,42 @@ export default function CardsPlant({ plants }: CardsPlantProps) {
         </Typography>
       ) : (
         plants.map((plant) => {
-          console.log(
-            plant,
-    plant.plantIconName,
-    PlantIcons.find((icon) => icon.name === plant.plantIconName)
-  )
-  return (
-          <Card key={plant.id} sx={{backgroundColor:'#f7f0e4'}}>
-            <CardContent
-            sx={{ display: 'flex', flexDirection: 'row' ,justifyContent: 'space-between', gap:1 }}
-             onClick={() =>{
-              navigate(`/projects/plants/${plant.id}`)} }>
-              <Box
-                component='img'
-                src={PlantIcons?.find((icon) => icon.name === plant.plantIconName)?.src || ''}
-                alt={plant.plantIconName}
-                sx={{
-                  width:'25%'
-                }}
-              >
-              </Box>
-              <Box sx={{ display: 'flex',flexDirection:'column', justifyContent: 'space-between', height: '100%', width:'75%'}}>
-              <Typography variant="h6" component="div">{plant.name.toLocaleUpperCase()}</Typography>
-              <Typography variant="body2" color="text.secondary">{plant.scientificName.toLocaleUpperCase()}</Typography>
-              <Box sx={{
-                fisplay:'flex',
-                flexWrap:'wrap',
-                justifyContent:'space-between',
-                width:'100%',
-                gap:1
-              }}>
-              <Typography variant="body2" color="text.secondary" sx={{fontSize:'12px', color:'gray'}}>Ph-Min: {plant.phMin}</Typography>
-              <Typography variant="body2" color="text.secondary"sx={{fontSize:'12px', color:'gray'}}>Ph-Max: {plant.phMax}</Typography>
-              <Typography variant="body2" color="text.secondary"sx={{fontSize:'12px', color:'gray'}}>Dias-Colheita: {plant.harvestMaxDays}</Typography>
-              </Box>
-              </Box> 
-            </CardContent>
-          </Card>
-        )})
+
+          return (
+            <Card key={plant.id} sx={{ backgroundColor: '#f7f0e4' }}>
+              <CardContent
+                sx={{ display: 'flex', flexDirection: 'row', justifyContent: 'space-between', gap: 1 }}
+                onClick={() => {
+                  navigate(`/projects/plants/${plant.id}`)
+                }}>
+                <Box
+                  component='img'
+                  src={PlantIcons?.find((icon) => icon.name === plant.plantIconName)?.src || ''}
+                  alt={plant.plantIconName}
+                  sx={{
+                    width: '25%'
+                  }}
+                >
+                </Box>
+                <Box sx={{ display: 'flex', flexDirection: 'column', justifyContent: 'space-between', height: '100%', width: '75%' }}>
+                  <Typography variant="h6" component="div">{plant.name.toLocaleUpperCase()}</Typography>
+                  <Typography variant="body2" color="text.secondary">{plant.scientificName.toLocaleUpperCase()}</Typography>
+                  <Box sx={{
+                    fisplay: 'flex',
+                    flexWrap: 'wrap',
+                    justifyContent: 'space-between',
+                    width: '100%',
+                    gap: 1
+                  }}>
+                    <Typography variant="body2" color="text.secondary" sx={{ fontSize: '12px', color: 'gray' }}>Ph-Min: {plant.phMin}</Typography>
+                    <Typography variant="body2" color="text.secondary" sx={{ fontSize: '12px', color: 'gray' }}>Ph-Max: {plant.phMax}</Typography>
+                    <Typography variant="body2" color="text.secondary" sx={{ fontSize: '12px', color: 'gray' }}>Dias-Colheita: {plant.harvestMaxDays}</Typography>
+                  </Box>
+                </Box>
+              </CardContent>
+            </Card>
+          )
+        })
       )}
     </Box>
   );
